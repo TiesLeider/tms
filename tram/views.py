61611592559ddd3e64fda6cbe48874a6876abfc4
Tiesl <Tycianol@gmail.com>
2019-10-17 13:13:12 +0200

    Eerste view

--- tram/views.py
+++ tram/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import LogoMelding
 
 # Create your views here.
+
+def index(request):
+    context = LogoMelding.objects.all()
+    list = []
+
+    for item in context:
+        list.append(item.assetnummer)
+    return HttpResponse(list)
