f071252ce0d594f0c49662ad9f5618fa2c1f5303
Ties <tycianol@gmail.com>
2020-05-20 10:24:38 +0200

    no asset matching query fix

--- tram/views/systeem.py
+++ tram/views/systeem.py
@@ -1,6 +1,7 @@
 import json
 import logging
 from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 
 logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s',
                     datefmt='%m/%d/%Y %I:%M:%S %p', filename="systeem.log", level=logging.INFO)
@@ -12,3 +13,4 @@ def error(request):
     data = str(request.body)[2:-1]
     json_data = json.loads(data).get("ojson")
     logging.error(f"{error}")
+    return JsonResponse({"response": True, "error": None})
